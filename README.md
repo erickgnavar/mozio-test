dc900cf968c88842c0c16d1f1ccb71c884109068
Erick Navarro <erick@navarro.io>
2018-04-22 20:44:59 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Mozio backend project
+
+Mozio Backend Potential hire Project 2.0
+
+# Tech stack
+
+## Backend
+
+- Python 3.6
+- Django 2.0
+- Postgres 10
---
+++ mozio/users/tests/test_models.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+
+
+from ..models import User
+
+
+class UserTestCase(TestCase):
+
+    def test_str(self):
+        user = User(username='test')
+        self.assertEqual(str(user), 'test')
